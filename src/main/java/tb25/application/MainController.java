fd71f2824bd34fe85530318f862e9eb2d304040a
TravidB <travisbu12345@gmail.com>
2025-11-12 14:41:54 -0800

    added button template

--- src/main/java/tb25/application/MainController.java
+++ src/main/java/tb25/application/MainController.java
@@ -1,4 +1,21 @@
 package tb25.application;
 
+import javafx.fxml.FXML;
+
 public class MainController {
+
+    @FXML
+    protected void onBrowseNovelClick(){
+
+    }
+
+    @FXML
+    protected void onAddNovelClick(){
+
+    }
+
+    @FXML
+    protected void onRemoveNovelClick(){
+
+    }
 }
