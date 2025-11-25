aab9d27cbe1c8e0494e586c18af555616f31fc6f
TravidB <travisbu12345@gmail.com>
2025-11-25 14:42:04 -0800

    added fields in addNovelController

--- src/main/java/tb25/application/addNovelController.java
+++ src/main/java/tb25/application/addNovelController.java
@@ -1,10 +1,29 @@
 package tb25.application;
 
 import javafx.fxml.FXML;
-import javafx.scene.control.TextField;
+import javafx.scene.control.*;
 
 public class addNovelController {
 
     @FXML
     private TextField novel_name;
+
+    @FXML
+    private TextField novel_author;
+
+    @FXML
+    private TextField novel_chapter;
+
+    @FXML
+    private TextField novel_page;
+
+    @FXML
+    private ToggleGroup progress_tracker;
+
+    @FXML
+    private Button add_button;
+
+    @FXML
+    private Button back_button;
+
 }
